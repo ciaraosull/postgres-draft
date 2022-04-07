cafc99f16cf0019c247bd2d80ffe79bfeb104e1f
ciaraosull <ciaraosull@gmail.com>
2022-04-07 19:05:56 +0000

    Create new sql-expression file and set up imports

---
+++ sql-expression.py
@@ -0,0 +1,8 @@
+from sqlalchemy import (
+    create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
+)
+
+# executing the instructions from our localhost "chinook" db
+db = create_engine("postgresql:///chinook")
+
+meta = MetaData(db)
