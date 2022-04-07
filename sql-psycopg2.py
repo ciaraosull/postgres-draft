2df20dec8024ad33ec1ad7da6071f83485b0fab6
ciaraosull <ciaraosull@gmail.com>
2022-04-07 18:53:50 +0000

    Add more queries to test

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -17,7 +17,7 @@ cursor.execute('SELECT * FROM "Artist"')
 # cursor.execute('SELECT "Name" FROM "Artist"')
 
 # Query 3 - select only "Queen" from the "Artist" table
-cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
 
 # Query 4 - select only by "ArtistId" #51 from the "Artist" table
 # cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
@@ -28,6 +28,13 @@ cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
 # Query 6 - select all tracks where the composer is
 # "Queen" from the "Track"table
 # cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# Challenge Query 7 - select only by "ArtistId" #3 from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [3])
+
+#  Challenge Query 8 - select all tracks where the composer is
+# "Blur" from the "Track"table - this shouldn't exist
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Blurr"])
 
 # fetch the results (multiple)
 results = cursor.fetchall()
